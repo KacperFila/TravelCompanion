972f30921d98d8c71ce80f761c385c8c169fdd3f
Kacper Fila <kacperfila26@gmail.com>
2024-02-22 13:27:53 +0100

    change postcards status add

---
+++ src/Modules/Travels/TravelCompanion.Modules.Travels.Api/Endpoints/Postcards/ChangePostcardStatus/ChangePostcardStatus.cs
@@ -0,0 +1,35 @@
+﻿using Ardalis.ApiEndpoints;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Swashbuckle.AspNetCore.Annotations;
+using TravelCompanion.Modules.Travels.Api.Endpoints.Travels;
+using TravelCompanion.Modules.Travels.Core.Services.Abstractions;
+
+namespace TravelCompanion.Modules.Travels.Api.Endpoints.Postcards.ChangePostcardStatus;
+
+[Route(TravelsModule.BasePath)]
+internal sealed class ChangePostcardStatus : EndpointBaseAsync
+    .WithRequest<ChangePostcardStatusRequest>
+    .WithActionResult
+{
+    private readonly IPostcardService _postcardService;
+
+    public ChangePostcardStatus(IPostcardService postcardService)
+    {
+        _postcardService = postcardService;
+    }
+
+    [HttpPut("Postcard/Status/{postcardId:guid}")]
+    [SwaggerOperation(
+        Summary = "Change Postcard Status By Id",
+        Tags = new[] { TravelsEndpoint.PostcardsTag })]
+    [ProducesResponseType(StatusCodes.Status204NoContent)]
+    [ProducesResponseType(typeof(void), StatusCodes.Status401Unauthorized)]
+    [ProducesResponseType(typeof(void), StatusCodes.Status403Forbidden)]
+    [ProducesResponseType(typeof(void), StatusCodes.Status404NotFound)]
+    public override async Task<ActionResult> HandleAsync(ChangePostcardStatusRequest request, CancellationToken cancellationToken = default)
+    {
+        await _postcardService.ChangeStatus(request.PostcardId, request.PostcardStatus);
+        return NoContent();
+    }
+}
---
+++ src/Modules/Travels/TravelCompanion.Modules.Travels.Api/Endpoints/Postcards/ChangePostcardStatus/ChangePostcardStatusRequest.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TravelCompanion.Modules.Travels.Core.Entities.Enums;
+
+namespace TravelCompanion.Modules.Travels.Api.Endpoints.Postcards.ChangePostcardStatus;
+
+internal class ChangePostcardStatusRequest
+{
+    [FromRoute(Name="postcardId")] public Guid PostcardId { get; set; }
+    [FromBody] public PostcardStatus PostcardStatus { get; set; }
+}
