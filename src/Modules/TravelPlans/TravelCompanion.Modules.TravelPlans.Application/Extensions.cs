a97237207c93cbe4f0e2a713c55a2c8f86ed3c76
Kacper Fila <kacperfila26@gmail.com>
2024-02-24 10:13:01 +0100

    module setup

---
+++ src/Modules/TravelPlans/TravelCompanion.Modules.TravelPlans.Application/Extensions.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace TravelCompanion.Modules.TravelPlans.Application;
+
+public static class Extensions
+{
+    public static IServiceCollection AddApplication(this IServiceCollection services)
+    {
+        return services;
+    }
+}
---
+++ src/Modules/TravelPlans/TravelCompanion.Modules.TravelPlans.Infrastructure/Extensions.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace TravelCompanion.Modules.TravelPlans.Infrastructure;
+
+public static class Extensions
+{
+    public static IServiceCollection AddInfrastructure(this IServiceCollection services)
+    {
+        return services;
+    }
+}
