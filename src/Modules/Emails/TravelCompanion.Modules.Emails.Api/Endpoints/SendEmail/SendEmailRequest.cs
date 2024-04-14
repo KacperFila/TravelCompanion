b47e29640e3f32f74056625c6adb8ad50b468535
Kacper Fila <kacperfila26@gmail.com>
2024-04-14 14:01:30 +0200

    emails and notifications module fix

--- src/Modules/Emails/TravelCompanion.Modules.Emails.Api/Endpoints/SendEmail/SendEmailRequest.cs
+++
@@ -1,9 +0,0 @@
-﻿using TravelCompanion.Shared.Abstractions.Emails;
-
-namespace TravelCompanion.Modules.Emails.Api.Endpoints.SendEmail;
-
-public class SendEmailRequest
-{
-    public string Email { get; set; }
-    public List<string> Receivers { get; set; }
-}
--- src/Modules/Emails/TravelCompanion.Modules.Emails.Core/Commands/SendThrowbackEmails.cs
+++
@@ -1,4 +0,0 @@
-﻿using TravelCompanion.Shared.Abstractions.Commands;
-
-namespace TravelCompanion.Modules.Emails.Core.Commands;
-internal record SendThrowbackEmails() : ICommand;
