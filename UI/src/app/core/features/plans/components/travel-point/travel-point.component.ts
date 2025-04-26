9f46323c0a3925e83c5d9934ff5658e3f2a5d100
Kacper Fila <kacperfila26@gmail.com>
2025-04-26 15:38:40 +0200

    refactor: roadmap component adjustments

--- UI/src/app/core/features/plans/components/travel-point/travel-point.component.ts
+++ UI/src/app/core/features/plans/components/travel-point/travel-point.component.ts
@@ -1,8 +1,10 @@
 import {
+  AfterViewInit,
   Component,
+  ElementRef,
   EventEmitter,
   Input,
-  Output
+  Output, ViewChild
 } from '@angular/core';
 import {
   TravelPoint, TravelPointUpdateRequest,
@@ -20,12 +22,31 @@ import {ItemListComponent} from "../../../../shared/item-list/item-list.componen
   standalone: true,
   imports: [CommonModule, ModalComponent, FormsModule, ItemListComponent],
 })
-export class TravelPointComponent {
+export class TravelPointComponent implements AfterViewInit {
   constructor(private plansService: PlansService) { }
+
+  @ViewChild('circle') circleElement!: ElementRef;
+
+  ngAfterViewInit() {
+    this.setCircleDimensions();
+    window.addEventListener('resize', this.setCircleDimensions.bind(this)); // Update on window resize
+  }
+
+  ngOnDestroy() {
+    window.removeEventListener('resize', this.setCircleDimensions.bind(this)); // Clean up listener
+  }
+
+  private setCircleDimensions() {
+    const circle = this.circleElement.nativeElement;
+    const width = circle.offsetWidth; // Get the current width
+    circle.style.height = `${width}px`; // Set the height to match the width
+  }
 
   @Input() travelPoint: TravelPoint = { id: '', placeName: '', totalCost: 0, travelPlanOrderNumber: 0 };
   @Input() updateRequests: TravelPointUpdateRequest[] = [];
   @Input() nodeNumber: number = 0;
+  @Input() showLineBefore: boolean = false;
+  @Input() showLineAfter: boolean = false;
 
   @Output() pointDeletedEvent = new EventEmitter<TravelPoint>();
   @Output() openEditRequestsModalEvent = new EventEmitter<TravelPointUpdateRequest[]>();
