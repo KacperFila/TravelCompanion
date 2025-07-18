73fceeb2db3f7feed5767652d4f7e257f2afbee1
Kacper Fila <kacperfila26@gmail.com>
2025-07-18 18:59:45 +0200

    feat: env var replacement script add

---
+++ UI/scripts/set-env.js
@@ -0,0 +1,25 @@
+const fs = require("fs");
+const path = require("path");
+
+try {
+  const apiUrl = process.env.API_URL || "";
+
+  const targetPath = path.resolve(
+    __dirname,
+    "../src/environments/environment.prod.ts"
+  );
+
+  const fileContent = `
+        export const environment = {
+        production: true,
+        API_URL: '${apiUrl}'
+        };
+        `;
+
+  fs.writeFileSync(targetPath, fileContent);
+  console.log("✅ Successfully set up environment settings");
+} catch (error) {
+  console.error("❌ Failed to set up environment settings");
+  console.error(error);
+  process.exit(1);
+}
